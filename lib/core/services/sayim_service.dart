1fbd777a15659c1b8eed8309716ace911ab3496a
Halil <nycgamedevs@gmail.com>
2026-07-11 03:54:21 +0300

    v0.0.16

--- lib/core/services/sayim_service.dart
+++ lib/core/services/sayim_service.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import '../models/sayim.dart';
+import '../models/davet.dart';
 
 class SayimService {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
