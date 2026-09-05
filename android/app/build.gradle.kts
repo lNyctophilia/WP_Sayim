6047e2f538d189d8666e393f5710d2310d14abeb
Halil <nycgamedevs@gmail.com>
2026-09-05 06:54:22 +0300

    Web build guncellendi - Cmt 05.09.2026  6:54

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -16,7 +16,6 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
-        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
@@ -45,8 +44,4 @@ android {
 
 flutter {
     source = "../.."
-}
-
-dependencies {
-    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
 }
